99372e9eb98f8d1bf5a36008e23c566387f33413
Saad Bakht <rotk13@gmail.com>
2014-07-20 05:48:43 -0400

    refactored loading level from args into seperate file

--- Main.rb
+++ Main.rb
@@ -2,36 +2,17 @@
 
 require_relative 'player'
 require_relative 'Board'
+require_relative 'LoadLevel'
 
 require 'io/console'
 
-levelArray = []
-row = 0
-col = 0
+brd = nil
+ply = nil
+
+# if CL argument exists
 if ARGV[0]
-	File.open(ARGV[0]).each_line do |line|
-	  levelArray.push line
-	end
-
-	# find the player start location so it can be set when creating player object
-	foundPlayer = false
-	levelArray.size.times do 
-	  col = 0
-	  levelArray[row].size.times do 
-	    if levelArray[row][col] == 'A'
-	      foundPlayer = true
-	      break
-	    end
-	    col += 1
-	  end
-	  break if foundPlayer == true
-	  row += 1
-	end
-
-	# creates board with empty array, sets board after
-	brd = Board.new(0)
-	brd.set_board(levelArray)
-	ply = Player.new(col,row,brd.get_board())
+	# creates board and player from level passed through CL arguments
+	brd, ply = load_level_from_args
 else
 
 	# the size of the array will be n x n
