0f647711c0131f1fbbba8e0e58d8cf86f59d37b4
Richard Hamilton <richardgregoryhamilton@gmail.com>
2014-11-17 18:38:06 -0600

    Refactored code

--- LoadLevel.rb
+++ LoadLevel.rb
@@ -1,14 +1,12 @@
-
 def load_level_from_args
 
   levelArray = []
   row = 0
   col = 0
-	File.open(ARGV[0]).each_line do |line|
-	  levelArray.push line
-	end
+	File.open(ARGV[0]).each_line {|line| levelArray.push line}
 
-	# find the player start location so it can be set when creating player object
+	# Find the player start location so it can be set when creating player object
+	
 	foundPlayer = false
 	levelArray.size.times do 
 	  col = 0
@@ -23,7 +21,8 @@ def load_level_from_args
 	  row += 1
 	end
 
-	# creates board with empty array, sets board after
+	# Creates board with empty array, sets board after
+	
 	brd = Board.new(0)
 	brd.set_board(levelArray)
 	ply = Player.new(col,row,brd.get_board())
--- Player.rb
+++ Player.rb
@@ -1,11 +1,11 @@
 require_relative 'character'
 
-
 class Player < Character
 
   @score = 0
 
-	# sets the initial position of the player, by sending parameters
+  # Sets the initial position of the player, by sending parameters
+	
   def initialize( x, y, board )
     set_x_position(x)
     set_y_position(y)
@@ -14,34 +14,26 @@ class Player < Character
       @score = 0
   end
 
-
-	# accepts user input and moves
+  # Accepts user input and moves
+	
   def move(key)
-    get_board[get_y_pos][get_x_pos] = ' ' # sets the current position to empty string (star eaten) before moving
+    get_board[get_y_pos][get_x_pos] = ' '                  # Sets the current position to empty string (star eaten) before moving
     case key 
-      when "w"
-        move_up
-      when "a"
-        move_left
-      when "d"
-        move_right
-      when "s"
-        move_down
-      else
-        puts "I have no idea what to do with that."
-    end # ends case
+      when "w" then move_up
+      when "a" then move_left
+      when "d" then move_right
+      when "s" then move_down
+      else puts "I have no idea what to do with that."
+    end                                                    # Ends case
     eatStar
-    draw_in_board('A') # sets the current position to A
+    draw_in_board('A')                                     # Sets the current position to A
   end
 
-  # eat a star, deleting it from the level and adding to player score
+  # Eat a star, deleting it from the level and adding to player score
+  
   def eatStar
-    if get_board[get_y_pos][get_x_pos] == '*'
-      @score += 1
-    end
+    @score += 1 if get_board[get_y_pos][get_x_pos] == '*'
     puts "Score : " + @score.to_s
   end
 
-
 end
-
