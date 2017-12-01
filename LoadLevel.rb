ace8f8e5b944bd321cab86855fe70aba48d4b949
Alyssa McKee <alyssa_mckee@yahoo.com>
2017-12-01 14:59:04 -0600

    Added Win condition. Game ends when all the stars are eaten

--- LoadLevel.rb
+++ LoadLevel.rb
@@ -3,28 +3,27 @@ def load_level_from_args
   levelArray = []
   row = 0
   col = 0
+  count = 0
 	File.open(ARGV[0]).each_line {|line| levelArray.push line}
 
 	# Find the player start location so it can be set when creating player object
 	
 	foundPlayer = false
-	levelArray.size.times do 
-	  col = 0
-	  levelArray[row].size.times do 
-	    if levelArray[row][col] == 'A'
-	      foundPlayer = true
-	      break
+	levelArray.size.times do |y|
+	  levelArray[y].size.times do |x|
+	    if levelArray[y][x] == 'A'
+		  col = x
+          row = y
 	    end
-	    col += 1
+		count +=1 if levelArray[y][x] == "*"
 	  end
-	  break if foundPlayer == true
-	  row += 1
 	end
 
 	# Creates board with empty array, sets board after
 	
 	brd = Board.new(0)
 	brd.set_board(levelArray)
+	brd.star_count = count
 	ply = Player.new(col,row,brd.get_board())
 
   return brd, ply
